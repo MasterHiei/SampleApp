c331eca00d5047d1a42a933bedb712cef0f524b1
MasterHiei <ruhrg.lcx@gmail.com>
2018-07-10 22:22:29 +0900

    include and apply slim template

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module SampleApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    # Change the template engine to slim
+    config.generators.template_engine = :slim
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
